a1ef341e3217ea1cf4191c66b7e468d2363db2c3
Mathijs Bernson <mathijs.bernson@gmail.com>
2016-01-06 19:29:13 +0100

    Initial Commit

---
+++ LEDControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LEDControl
+//
+//  Created by Mathijs Bernson on 06/01/16.
+//  Copyright © 2016 Duckson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
